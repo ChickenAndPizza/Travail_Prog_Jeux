3b6508fa183ae98e8de67bd5917ecad966ab024b
B533P-10\533 <simoroy94@gmail.com>
2018-09-10 16:03:34 -0400

    Animation jump , running et idle

---
+++ 7Blessing/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    private Animator mAnimator;
+	// Use this for initialization
+	void Start () {
+        mAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool runningRight = Input.GetKey(KeyCode.D);
+        bool runningLeft = Input.GetKey(KeyCode.A);
+        bool jumpingRight = Input.GetKey(KeyCode.Space);
+
+        mAnimator.SetBool("RunningRight", runningRight);
+        mAnimator.SetBool("RunningLeft", runningLeft);
+        mAnimator.SetBool("JumpingRight", jumpingRight);
+    }
+}
