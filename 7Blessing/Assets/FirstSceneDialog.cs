b5161f4dc389d221e0d0071e2830f964e95e8590
B533P-04\533 <jeepsboucher@gmail.com>
2018-09-17 17:13:21 -0400

    Ajout des interactions et du dialog de départ

---
+++ 7Blessing/Assets/FirstSceneDialog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSceneDialog : MonoBehaviour {
+    private bool havePlay;
+    DialogManager dialogManager;
+    [SerializeField] DialogText dialogText;
+    // Use this for initialization
+    void Awake()
+    {
+        dialogManager = GameObject.FindGameObjectWithTag("DialogManager").GetComponent<DialogManager>();
+        dialogManager.StartDialog(dialogText);
+
+    }
+}
