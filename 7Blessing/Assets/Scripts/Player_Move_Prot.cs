8934c592d4ff0cbae8952ccf76a61bc18cd0e752
B533P-09\533 <simoroy94@gmail.com>
2018-09-24 16:59:02 -0400

    Correction du bug de grounded

--- 7Blessing/Assets/Scripts/Player_Move_Prot.cs
+++ 7Blessing/Assets/Scripts/Player_Move_Prot.cs
@@ -7,42 +7,49 @@ public class Player_Move_Prot : MonoBehaviour {
 
     public int playerSpeed = 10;
     public int playerJumpPower = 7;
-    public float moveX = 2;
+    public float moveX = 0;
     public bool grounded = false;
     public bool facingRight = true;
     private bool jumping = false;
     private bool running = false;
     private bool controlAreEnable = true;
+    public Transform GroundCheck;
+    public LayerMask groundLayer;
     public bool eIsPressed = false;
+    private Animator mAnimator;
+    private Rigidbody2D mPlayerBody;
     // Use this for initialization
     void Start()
     {
-
+        mAnimator = GetComponent<Animator>();
+        mPlayerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(controlAreEnable)
+        grounded = IsGrounded();
+        if (controlAreEnable)
         {
             if (Input.GetAxis("Jump")!= 0 && grounded)
             {
                 Jump();
-                grounded = false;
             }
-        if(Input.GetAxis("Horizontal") != 0)
-        {
-            running = true;
-            MovePlayer();
-            ManageInteraction();
-        }
+            if(Input.GetAxis("Horizontal") != 0)
+            {
+                running = true;
+                MovePlayer();
+                ManageInteraction();
+            }
+            else
+            {
+                running = false;
+            }
 
         }
-        else
-        {
-            running = false;
-        }
+
         mAnimator.SetBool("Running", running);
+        mAnimator.SetBool("Grounded", grounded);
         mAnimator.SetBool("JumpingRight", jumping);
     }
 
@@ -58,6 +65,24 @@ public class Player_Move_Prot : MonoBehaviour {
             FlipPlayer();
         }
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        mAnimator.SetFloat("VelocityY", gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        
+    }
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 0.5f;
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if(hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+        // Vector3 vector = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        // Debug.DrawRay(vector, Vector3.down,Color.red);
+        // int numberOfCollision = mPlayerBody.Cast(Vector2.down * 0.05f, results);
     }
 
     void FlipPlayer()
@@ -76,7 +101,6 @@ public class Player_Move_Prot : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            grounded = true;
             jumping = false;
             gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
