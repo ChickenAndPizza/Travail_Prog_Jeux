b7a7a69a266abe6193070d234dddd1bfb45b1b45
B533P-05\533 <alessard123@hotmaill.com>
2018-10-05 16:10:15 -0400

    Fire ball

--- 7Blessing/Assets/Scripts/Enemies/RedEnemy.cs
+++ 7Blessing/Assets/Scripts/Enemies/RedEnemy.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [DisallowMultipleComponent]
-public class Enemy_movements : MonoBehaviour {
+public class RedEnemy : MonoBehaviour {
+
     [SerializeField] Vector2 movementVector = new Vector2(10f, 10f);
     [SerializeField] float period = 2f;
 
@@ -13,11 +14,28 @@ public class Enemy_movements : MonoBehaviour {
     Vector2 startingPos;
 
 
-    void Start () {
+    void Start()
+    {
         startingPos = transform.position;
     }
-	
-	void Update () {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*Damagable damagable = collision.gameObject.GetComponent<Damagable>();
+        if (damagable != null)
+        {
+            damagable.DealDamage(bulletDamage);
+        }*/
+
+        /*SI JOUEUR COLLIDER : */
+        /*if (collision.gameObject.tag == "Player")
+        {
+            Invoke("die", DestroyFireBallAfterCollisionTime);
+        }*/
+    }
+
+    void Update()
+    {
         if (period <= Mathf.Epsilon)
         {
             return;
@@ -25,7 +43,7 @@ public class Enemy_movements : MonoBehaviour {
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2;
         float rawSinWave = Mathf.Sin(cycles * tau);
-        Vector2 offset = movementVector * rawSinWave /120;
+        Vector2 offset = movementVector * rawSinWave / 120;
         transform.position = startingPos + offset;
 
     }
---
+++ 7Blessing/Assets/Scripts/Enemies/RedEnemy_fireball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemy_fireball : MonoBehaviour {
+
+    /*Rigidbody rigidBody;*/
+    [SerializeField] float DestroyFireBallAfterCollisionTime;
+    [SerializeField] float maxLifeTime = 5f;
+    [SerializeField] float FireBallForce = 1500;
+    [SerializeField] int FireBallDamage = 20;
+
+    void Start()
+    {
+        /*rigidBody = GetComponent<Rigidbody>();
+        Vector3 ajustedMovement = transform.TransformDirection(Vector3.forward);
+        rigidBody.AddForce(ajustedMovement * FireBallForce);*/
+        Invoke("DestroyFireBall", maxLifeTime);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.right * -FireBallForce * Time.deltaTime;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*Damagable damagable = collision.gameObject.GetComponent<Damagable>();
+        if (damagable != null)
+        {
+            damagable.DealDamage(bulletDamage);
+        }*/
+
+        /* SI JOUEUR COLLIDER : */
+        print(collision.gameObject.tag);
+        print(collision.gameObject.name);
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("DestroyFireBall", DestroyFireBallAfterCollisionTime);
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            Invoke("DestroyFireBall", DestroyFireBallAfterCollisionTime);
+        }
+    }
+
+    private void DestroyFireBall()
+    {
+        Destroy(gameObject);
+    }
+
+}
