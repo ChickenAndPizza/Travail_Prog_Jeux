177ec93738570c9136745d7cd8c664de892101ba
B533P-04\533 <jeepsboucher@gmail.com>
2018-11-16 16:11:06 -0500

    Commencement du menu final et du dialogue

---
+++ 7Blessing/Assets/ConclusionDialog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConclusionDialog : MonoBehaviour {
+    DialogManager dialogManager;
+    [SerializeField] DialogText dialogText;
+    [SerializeField] string nextSceneName;
+    // Use this for initialization
+    void Awake()
+    {
+        dialogManager = GameObject.FindGameObjectWithTag("DialogManager").GetComponent<DialogManager>();
+        dialogManager.StartDialog(dialogText);
+    }
+
+}
---
+++ 7Blessing/Assets/EndingMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingMenu : MonoBehaviour {
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
